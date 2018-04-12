65059ffc68907a674474ac27d17e893b7622fa70
fabianramos01 <fabian.ramos01@uptc.edu.co>
2018-04-11 21:58:09 -0500

    Genera el arbol de archivos y lo imprime por consola

--- src/models/ManagerFiles.java
+++ src/models/ManagerFiles.java
@@ -15,7 +15,8 @@ public class ManagerFiles {
 	public void loadTree(File file, int num) {
 		nTree.addNode(null, new NodeFather(file.getName()));
 		loadFiles(file, num);
-//		nTree.print();
+		System.out.println("---");
+		nTree.print();
 	}
 
 	private void loadFiles(File actual, int num) {
@@ -34,22 +35,21 @@ public class ManagerFiles {
 	private void extensionExist(File file, int num) {
 		String name = file.getName();
 		if (name.lastIndexOf(".") == -1) {
-			name += ConstantList.OTHER;	
+			name += ConstantList.OTHER;
 		}
 		int i = name.lastIndexOf(".");
-		Node node = nTree.search(name.substring(i));
-		System.out.println(name.substring(i));
-		if (node != null) {
-			if (file.length() < num) {
-				nTree.addNode(node.getChilds().get(0), new NodeFather(name.substring(0, i)));
-			} else {
-				nTree.addNode(node.getChilds().get(1), new NodeFather(name.substring(0, i)));
-			}
+		Node node = nTree.search(name.substring(i+1));
+		while (node == null) {
+			createDirectory(name.substring(i + 1), num);
+			node = nTree.search(name.substring(i+1));
+		}
+		if (file.length() < num) {
+			nTree.addNode(node.getChilds().get(0), new NodeFather(name.substring(0, i)));
 		} else {
-			createDirectory(name.substring(i+1), num);
+			nTree.addNode(node.getChilds().get(1), new NodeFather(name.substring(0, i)));
 		}
 	}
-	
+
 	private void createDirectory(String folderName, int num) {
 		Node extension = new NodeFather(folderName);
 		nTree.addNode(extension, new NodeFather(ConstantList.FOLDER_SMALL + num));
